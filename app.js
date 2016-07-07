56d42b975ed23d2a886e4051a72c5ae9500898da
gmittal <gautam@mittal.net>
2016-07-07 10:47:57 -0700

    more deps

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // Copyright 2016 Project Copilot
 
 var port = 3000;
+var bodyParser = require('body-parser');
+var colors = require('colors');
 var express = require('express');
 var app = express();
 
@@ -9,6 +11,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+/* CORE API ENDPOINTS */
+
+/*
+  POST /api/request -- takes input from HTML form in
+  the user-client and adds it to a database (currently mongodb)
+*/
+app.post('/api/request', function (req, res) {
+
+});
+
 app.listen(port, function () {
-  console.log('Copilot Core Services running.');
+  console.log(('Copilot Core Services running at ').blue + ('localhost:'+port).magenta);
 });

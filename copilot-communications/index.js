4d8f036af8e231ec73535d3797020fd197a16ac9
gmittal <gautam@mittal.net>
2016-07-07 14:00:34 -0700

    updates to comm systems

--- copilot-communications/index.js
+++ copilot-communications/index.js
@@ -1,3 +1,6 @@
+// Project Copilot Communcations Systems
+// Seamlessly handles all communications between users and volunteers
+
 exports.printMsg = function() {
   console.log("This is a message from the communcations systems package");
 }

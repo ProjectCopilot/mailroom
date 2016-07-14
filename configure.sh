c7a5c0636b31072b8d20fb764bdb05e7d5d74ea2
Ankit Ranjan <me@ankit.io>
2016-07-14 15:20:40 -0700

    Separated build and deploy scripts

--- configure.sh
+++ configure.sh
@@ -5,13 +5,13 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG=$BASEDIR/.env
 
 echo "What hostname do you want to run the app on?"
-read HOST
+read HOST >$(tty)
 echo "What port do you want to run the listener on?"
-read PORT
+read PORT >$(tty)
 echo "What is the hash length?"
-read HASH_LENGTH
+read HASH_LENGTH >$(tty)
 echo "What is the hash salt?"
-read HASH_SALT
+read HASH_SALT >$(tty)
 
 cat <<EOF > $CONFIG
 HOSTNAME=$HOST

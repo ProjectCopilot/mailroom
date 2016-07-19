84df5a45524b5d025f268a5ec8601b76e484787b
gmittal <gautam@mittal.net>
2016-07-19 14:04:25 -0700

    Communications send functionality implemented for both email and SMS

--- copilot-communications/index.js
+++ copilot-communications/index.js
@@ -2,14 +2,16 @@
   * Project Copilot Communications Sysmte
   * Seamlessly handles all communication between volunteers and users
 */
+var colors = require('colors');
 var dotenv = require('dotenv').config({path: __dirname+'/../.env'});
-var sms = new require('twilio').RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-var email = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
+var twilio = require('twilio')
+var sms = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+var email = require('sendgrid')(process.env.SENDGRID_API_KEY)
 var fs = require('fs');
 
 var exports = module.exports = {};
 
-exports.send = function(type, contact, subject, body) {
+exports.send = function(type, contact, body, subject) {
   if (type.toLowerCase() == "email") {
 
     fs.readFile(__dirname+'/../templates/message.html', 'utf-8', function (err, data) {
@@ -33,7 +35,7 @@ exports.send = function(type, contact, subject, body) {
       body: body
     }, function (e, m) {
       if (!e) {
-        console.log("Successfully sent SMS with SID ".green, (message.sid).magenta);
+        console.log("Successfully sent SMS with SID".green, (m.sid).magenta);
       } else {
         console.log("Error sending SMS message to ".red+(contact).blue);
       }

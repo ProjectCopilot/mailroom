be353c604bea81add4a59035aea91a37cdc0c7fe
Gautam Mittal <gautam@mittal.net>
2016-07-21 11:12:13 -0700

    update build.sh with npm install (#18)

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -eo pipefail
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 npm install --prefix $BASEDIR $BASEDIR
-
+npm install --prefix $BASEDIR/copilot-communications
+npm install --prefix $BASEDIR/copilot-prioritize

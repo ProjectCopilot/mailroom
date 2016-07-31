e7d66bf7b386e71d184741b707231a8e508a3feb
Ankit Ranjan <ranjanankit@gmail.com>
2016-07-31 10:52:45 -0700

    Linting (#49)
    * Install eslint
    
    * Basic linting

--- copilot-prioritize/index.js
+++ copilot-prioritize/index.js
@@ -4,12 +4,12 @@
   * Used to measure urgency of certain requests for volunteers to handle sooner than later
 */
 
-var exports = module.exports = {};
+exports = module.exports = {};
 
-exports.sort = function(l) {
-  var sorted = l.slice();
-  sorted = sorted.sort(function(a, b) {
+exports.sort = function (l) {
+  let sorted = l.slice();
+  sorted = sorted.sort(function (a, b) {
     return new Date(a.time_submitted).getTime() - new Date(b.time_submitted).getTime();
   });
   return sorted;
-}
+};

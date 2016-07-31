7e7e8350843743c4616507610521fab470f1be3b
Ankit Ranjan <me@ankit.io>
2016-07-31 01:03:14 -0700

    Basic linting

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 // Project Copilot Core Services
 // Copyright 2016 Project Copilot
 
-var app = require('express')();
-var bandname = require('bandname');
-var bodyParser = require('body-parser');
-var colors = require('colors');
-var communicate = require(__dirname+'/copilot-communications/index.js');
-var dotenv = require('dotenv').config({path: __dirname+'/.env'});
-var firebase = require('firebase');
-var hashid = require('hashids');
-var multiparty = require('multiparty');
-var prioritize = require(__dirname+'/copilot-prioritize/index.js');
+const app = require('express')();
+const bandname = require('bandname');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const communicate = require(__dirname + '/copilot-communications/index.js');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+const firebase = require('firebase');
+const hashid = require('hashids');
+const multiparty = require('multiparty');
+const prioritize = require(__dirname + '/copilot-prioritize/index.js');
 
 require('shelljs/global');
 
 /* SET UP */
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(function(req, res, next) { // enable CORS and assume JSON return structure
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) { // enable CORS and assume JSON return structure
   res.setHeader('Content-Type', 'application/json');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 // setup Firebase
 firebase.initializeApp({
-  serviceAccount: __dirname+'/'+process.env.FIREBASE_KEY_PATH,
-  databaseURL: "https://"+process.env.FIREBASE_ID+".firebaseio.com"
+  serviceAccount: __dirname + '/' + process.env.FIREBASE_KEY_PATH,
+  databaseURL: 'https://' + process.env.FIREBASE_ID + '.firebaseio.com',
 });
 
 // Initialize database
-var db = firebase.database().ref("/");
+const db = firebase.database().ref('/');
 
-var hash = new hashid(process.env.HASH_SALT, process.env.HASH_LENGTH);
+const hash = new hashid(process.env.HASH_SALT, process.env.HASH_LENGTH);
 
 
 /* CORE API ENDPOINTS */
@@ -44,7 +44,7 @@ var hash = new hashid(process.env.HASH_SALT, process.env.HASH_LENGTH);
 */
 app.post('/api/addUserRequest', function (req, res) {
   // specify post body schema
-  var schema = {
+  const schema = {
     referral: 'String',
     name: 'String',
     age: 0,
@@ -52,29 +52,28 @@ app.post('/api/addUserRequest', function (req, res) {
     school: 'String',
     contactMethod: 'String',
     contact: 'String',
-    situation: 'String'
-  }
+    situation: 'String',
+  };
 
   // validate the request body based on the specified schema
-  var checkParams = validateRequestParameters(schema, req.body);
+  const checkParams = validateRequestParameters(schema, req.body);
 
   // process entry
   if (checkParams.valid === true) {
-    var hasDuplicateCase = false;
+    let hasDuplicateCase = false;
 
     // check if a request with the same contact info has not already been submitted
-    db.child("cases").once("value", function (snapshot) {
+    db.child('cases').once('value', function (snapshot) {
       if (snapshot.val() !== null) {
-        for (var k in snapshot.val()) {
-          var numberPattern = /\d+/g;
+        for (const k in snapshot.val()) {
+          const numberPattern = /\d+/g;
 
-          if (req.body.contactMethod === "SMS") {
-
-            if ((snapshot.val()[k].contact).match(numberPattern).join("").substr(-10) === (req.body.contact).match(numberPattern).join("").substr(-10)) {
+          if (req.body.contactMethod === 'SMS') {
+            if ((snapshot.val()[k].contact).match(numberPattern).join('').substr(-10) === (req.body.contact).match(numberPattern).join('').substr(-10)) {
               hasDuplicateCase = true;
               break;
             }
-          } else if (req.body.contactMethod == "Email" && snapshot.val()[k].contact == req.body.contact) {
+          } else if (req.body.contactMethod == 'Email' && snapshot.val()[k].contact == req.body.contact) {
             hasDuplicateCase = true;
             break;
           }
@@ -86,33 +85,30 @@ app.post('/api/addUserRequest', function (req, res) {
         }
 
         var pendingRequest = req.body;
-        pendingRequest["display_name"] = bandname();
-        pendingRequest["time_submitted"] = new Date().getTime();
-        pendingRequest["helped"] = false;
+        pendingRequest['display_name'] = bandname();
+        pendingRequest['time_submitted'] = new Date().getTime();
+        pendingRequest['helped'] = false;
         var id = hash.encode(pendingRequest.time_submitted);
         console.log('New case submitted with ID: '.green + (id).magenta);
-        db.child("cases").child(id).set(req.body, function () {
+        db.child('cases').child(id).set(req.body, function () {
           res.status(200).end();
         });
-
       } else {
         var pendingRequest = req.body;
-        pendingRequest["display_name"] = bandname();
-        pendingRequest["time_submitted"] = new Date().getTime();
-        pendingRequest["helped"] = false;
+        pendingRequest['display_name'] = bandname();
+        pendingRequest['time_submitted'] = new Date().getTime();
+        pendingRequest['helped'] = false;
         var id = hash.encode(pendingRequest.time_submitted);
         console.log('New case submitted with ID: '.green + (id).magenta);
-        db.child("cases").child(id).set(req.body, function () {
+        db.child('cases').child(id).set(req.body, function () {
           res.status(200).end();
         });
       }
     });
-
   } else { // otherwise return error
-    console.log("/api/addUserRequest".cyan + " had bad request for: ".blue + (checkParams.reason).red);
+    console.log('/api/addUserRequest'.cyan + ' had bad request for: '.blue + (checkParams.reason).red);
     res.status(500).end();
   }
-
 });
 
 
@@ -120,13 +116,13 @@ app.post('/api/addUserRequest', function (req, res) {
   GET /api/getNewRequests -- using the prioritize function, return the most urgent inquiries
   No schema necessary.
 */
-app.get("/api/getRequests/:number", function (req, res) {
+app.get('/api/getRequests/:number', function (req, res) {
     // get the number of desired cases
-    var numRequests = req.params.number;
+  const numRequests = req.params.number;
 
-    db.child("cases").orderByChild("time_submitted").limitToFirst(parseInt(numRequests, 10)).once("value", function (snapshot) {
-      res.send(snapshot.val())
-    });
+  db.child('cases').orderByChild('time_submitted').limitToFirst(parseInt(numRequests, 10)).once('value', function (snapshot) {
+    res.send(snapshot.val());
+  });
 });
 
 
@@ -135,90 +131,87 @@ app.get("/api/getRequests/:number", function (req, res) {
 */
 
 // If a new message comes from a volunteer, route that to the user
-db.child("cases").on("value", function (snap) {
-    for (var k in snap.val()) {
-      for (var m in snap.val()[k].messages) {
-        if (snap.val()[k].messages[m].sender === "volunteer" && snap.val()[k].messages[m].sent === false) {
-          var method = snap.val()[k].contactMethod;
-          var contact = snap.val()[k].contact;
-          var body = snap.val()[k].messages[m].body;
-          var subject = "New Message";
-          communicate.send(method, contact, body, subject);
-          db.child("cases").child(k).child("messages").child(m).child("sent").set(true);
-        }
+db.child('cases').on('value', function (snap) {
+  for (const k in snap.val()) {
+    for (const m in snap.val()[k].messages) {
+      if (snap.val()[k].messages[m].sender === 'volunteer' && snap.val()[k].messages[m].sent === false) {
+        const method = snap.val()[k].contactMethod;
+        const contact = snap.val()[k].contact;
+        const body = snap.val()[k].messages[m].body;
+        const subject = 'New Message';
+        communicate.send(method, contact, body, subject);
+        db.child('cases').child(k).child('messages').child(m).child('sent').set(true);
       }
     }
-
+  }
 });
 
 
 /* COMMUNICATION ENDPOINTS/WEBHOOKS */
 
 // Incoming email (SendGrid) webhook
-app.post('/communication/incoming/email', function(req, res) {
-  var form = new multiparty.Form();
-  form.parse(req, function(err, fields, files) {
-    var fromEmail = JSON.parse(fields.envelope).from;
-    var rawEmailBody = fields.text[0];
-    var fromHeader = fields.from[0];
+app.post('/communication/incoming/email', function (req, res) {
+  const form = new multiparty.Form();
+  form.parse(req, function (err, fields, files) {
+    const fromEmail = JSON.parse(fields.envelope).from;
+    const rawEmailBody = fields.text[0];
+    const fromHeader = fields.from[0];
 
-    var body = rawEmailBody;// stripEmail(rawEmailBody, fromHeader);
+    const body = rawEmailBody;// stripEmail(rawEmailBody, fromHeader);
 
-    var attachments = [];
-    for (var key in files) {
-      var fileInfo = files[key][0];
-      var path = fileInfo.path;
-      var uploadOutString = exec(__dirname+'/util/imgur.sh '+ path +' '+ process.env.IMGUR_CLIENT_ID, {silent:true}).stdout.replace(/\/n/g, "").trim();
-      var uploadResponse = uploadOutString.indexOf("Error: ENOSPC") > -1 ? false : uploadOutString;
+    const attachments = [];
+    for (const key in files) {
+      const fileInfo = files[key][0];
+      const path = fileInfo.path;
+      const uploadOutString = exec(__dirname + '/util/imgur.sh ' + path + ' ' + process.env.IMGUR_CLIENT_ID, { silent: true }).stdout.replace(/\/n/g, '').trim();
+      const uploadResponse = uploadOutString.indexOf('Error: ENOSPC') > -1 ? false : uploadOutString;
 
       if (uploadResponse !== false) {
         attachments.push(uploadResponse);
       }
-
     }
 
-    var message = {
-      "from": fromEmail,
-      "body": body,
-      "attachments": attachments,
-      "sender": "user"
+    const message = {
+      'from': fromEmail,
+      body,
+      attachments,
+      'sender': 'user',
     };
 
-    db.child("cases").once("value", function (s) {
-      for (var k in s.val()) {
-        if (s.val()[k]["contact"] == message.from) {
-          db.child("cases").child(k).child("messages").push(message);
+    db.child('cases').once('value', function (s) {
+      for (const k in s.val()) {
+        if (s.val()[k]['contact'] == message.from) {
+          db.child('cases').child(k).child('messages').push(message);
         }
       }
     });
 
     res.status(200).end();
-
   });
 });
 
 // Incoming SMS (Twilio) webhook
 app.post('/communication/incoming/sms', function (req, res) {
-  var attachments = [];
-  for (var i = 0; i < parseInt(req.body.NumMedia, 10); i++) {
-    attachments.push(req.body["MediaUrl"+i.toString()]);
+  const attachments = [];
+  for (let i = 0; i < parseInt(req.body.NumMedia, 10); i++) {
+    attachments.push(req.body['MediaUrl' + i.toString()]);
   }
 
-  var numberPattern = /\d+/g;
+  const numberPattern = /\d+/g;
 
-  var message = {
-    "from": (req.body.From).match(numberPattern).join("").substr(-10),
-    "body": req.body.Body,
-    "attachments": attachments,
-    "sender": "user"
-  }
+  const message = {
+    'from': (req.body.From).match(numberPattern).join('').substr(-10),
+    'body': req.body.Body,
+    attachments,
+    'sender': 'user',
+  };
 
 
-  db.child("cases").once("value", function (s) {
-    for (var k in s.val()) {
-      if (s.val()[k]["contact"].match(numberPattern) !== null) {
-        if (s.val()[k]["contact"].match(numberPattern).join("").substr(-10) == message.from) {
-          db.child("cases").child(k).child("messages").push(message);
+  db.child('cases').once('value', function (s) {
+    for (const k in s.val()) {
+      if (s.val()[k]['contact'].match(numberPattern) !== null) {
+        if (s.val()[k]['contact'].match(numberPattern).join('').substr(-10) == message.from) {
+          db.child('cases').child(k).child('messages').push(message);
         }
       }
     }
@@ -232,32 +225,31 @@ app.get('/up', function (req, res) {
     res.status(200).end();
 });
 
-
 /* HELPER FUNCTIONS */
 
 // given a body schema and the actual request body, return whether request body is valid or not and reason
 function validateRequestParameters(schema, body) {
-  var valid = true;
-  var reason = "None";
-  for (var field in schema) {
+  let valid = true;
+  let reason = 'None';
+  for (const field in schema) {
     if (!(field in body)) {
       valid = false;
-      reason = "Missing parameters.";
+      reason = 'Missing parameters.';
       break;
     }
-    if (typeof(schema[field]) == "string" && body[field].length == 0) {
+    if (typeof(schema[field]) == 'string' && body[field].length == 0) {
       valid = false;
-      reason = "Cannot pass empty string as parameter value.";
+      reason = 'Cannot pass empty string as parameter value.';
       break;
     }
-    if (typeof(schema[field]) == "number" && typeof(body[field]) == "string" && isNaN(parseInt(body[field], 10))) {
+    if (typeof(schema[field]) == 'number' && typeof(body[field]) == 'string' && isNaN(parseInt(body[field], 10))) {
       valid = false;
-      reason = "Cannot pass NaN value as numreic parameter value."
+      reason = 'Cannot pass NaN value as numreic parameter value.';
       break;
     }
   }
 
-  return {"valid": valid, "reason":reason};
+  return { valid, reason };
 }
 
 
@@ -268,6 +260,6 @@ function stripEmail(emailString, fromHeader) {
 
 
 app.listen(process.env.PORT, process.env.HOSTNAME, function () {
-  console.log(('Copilot Core Services running at ').blue + (process.env.HOSTNAME+":"+process.env.PORT).magenta);
-  console.log('Node '+exec('node --version', {silent:true}).stdout);
+  console.log(('Copilot Core Services running at ').blue + (process.env.HOSTNAME + ':' + process.env.PORT).magenta);
+  console.log('Node ' + exec('node --version', { silent: true }).stdout);
 });
--- copilot-communications/index.js
+++ copilot-communications/index.js
@@ -2,44 +2,42 @@
   * Project Copilot Communications Sysmte
   * Seamlessly handles all communication between volunteers and users
 */
-var colors = require('colors');
-var dotenv = require('dotenv').config({path: __dirname+'/../.env'});
-var twilio = require('twilio')
-var sms = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-var email = require('sendgrid')(process.env.SENDGRID_API_KEY)
-var fs = require('fs');
+const colors = require('colors');
+const dotenv = require('dotenv').config({ path: __dirname + '/../.env' });
+const twilio = require('twilio');
+const sms = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const email = require('sendgrid')(process.env.SENDGRID_API_KEY);
+const fs = require('fs');
 
-var exports = module.exports = {};
+exports = module.exports = {};
 
-exports.send = function(type, contact, body, subject) {
-  if (type.toLowerCase() == "email") {
+exports.send = function (type, contact, body, subject) {
+  if (type.toLowerCase() == 'email') {
+    fs.readFile(__dirname + '/../templates/message.html', 'utf-8', function (err, data) {
+      const emailBody = data.replace(/{HEADER-MESSAGE}/g, subject).replace(/{MESSAGE-BODY}/g, body);
 
-    fs.readFile(__dirname+'/../templates/message.html', 'utf-8', function (err, data) {
-        var emailBody = data.replace(/{HEADER-MESSAGE}/g, subject).replace(/{MESSAGE-BODY}/g, body);
-
-        email.send({
-          to:       contact,
-          from:     process.env.SENDGRID_EMAIL,
-          fromname: 'Project Copilot',
-          subject:  subject,
-          html:     emailBody,
-        }, function(err, json) {
-          if (err) { return console.error(err); }
-          console.log("Successfully sent email.".green);
-        });
+      email.send({
+        to: contact,
+        from: process.env.SENDGRID_EMAIL,
+        fromname: 'Project Copilot',
+        subject,
+        html: emailBody,
+      }, function (err, json) {
+        if (err) { return console.error(err); }
+        console.log('Successfully sent email.'.green);
+      });
     });
-
-  } else if (type.toLowerCase() == "sms") {
+  } else if (type.toLowerCase() == 'sms') {
     sms.sms.messages.create({
       to: contact,
       from: process.env.TWILIO_PHONE_NUMBER,
-      body: body
+      body,
     }, function (e, m) {
       if (!e) {
-        console.log("Successfully sent SMS with SID".green, (m.sid).magenta);
+        console.log('Successfully sent SMS with SID'.green, (m.sid).magenta);
       } else {
-        console.log("Error sending SMS message to ".red+(contact).blue);
+        console.log('Error sending SMS message to '.red + (contact).blue);
       }
     });
   }
-}
+};

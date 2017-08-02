19e11a9c5ee76aedc680e9b52d38a6c959487411
Gautam Mittal <gautam@mittal.net>
2017-08-02 09:20:34 -0700

    Added email conversation threading (#86)
    * Closes #80
    
    * Update index.js

--- copilot-communications/email/index.js
+++ copilot-communications/email/index.js
@@ -8,13 +8,19 @@ exports = module.exports = {};
 exports.send = (contact, body, subject) => {
   fs.readFile(`${__dirname}/../../templates/message.html`, 'utf-8', (err, data) => {
     const emailBody = data.replace(/{HEADER-MESSAGE}/g, subject).replace(/{MESSAGE-BODY}/g, body);
-    email.send({
+    var message = new email.Email({
       to: contact,
       from: process.env.SENDGRID_EMAIL,
       fromname: 'Project Copilot',
       subject,
-      html: emailBody,
-    }, (e) => {
+      html: emailBody
+    });
+
+    const conversation_id = new Buffer(contact).toString('base64');
+    message.addHeader({'In-Reply-To': `<${conversation_id}@support.copilot.help>`});
+    message.addHeader({'References': `<${conversation_id}@support.copilot.help>`});
+
+    email.send(message, (e, json) => {
       if (e) { return console.error(e); }
       console.log('Successfully sent email.');
     });
